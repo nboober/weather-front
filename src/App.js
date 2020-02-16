051372639f13902d15e214915750b2a2ed2b54a8
Nick Boober <nboober@terpmail.umd.edu>
2020-02-16 13:07:47 -0500

    successfully fetched test data from backend and created a method to get the coordinated for my geolocation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      sixdayforecast:"",
+      myLocation:"",
+    }
+  }
+
+  componentDidMount = () => {
+    fetch("http://localhost:3000/test")
+    .then(response => response.json())
+    .then(testDataArray => this.setState({
+      sixdayforecast: testDataArray
+      },()=>{
+        this.getGeoLocation();
+      })
+    )
+  }
+
+  getGeoLocation = () => {
+    const showPosition = (position) => {
+      // console.log(position.coords.latitude);
+      // console.log(position.coords.longitude);
+        
+        this.setState({
+          myLocation: `${position.coords.latitude},${position.coords.longitude}`
+        })
+      
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
   render(){
     return(
       <div>
