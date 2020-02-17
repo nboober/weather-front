d39d984064488b205c8fd155af27698e203dec74
Nick Boober <nboober@terpmail.umd.edu>
2020-02-17 13:21:22 -0500

    day components now show the temps, humidity, and wind speed for that day

--- src/children/Day.js
+++ src/children/Day.js
@@ -35,6 +35,15 @@ class Day extends React.Component{
             <div style={{display: "inline-block", margin: "20px"}} >
                 <img src={this.props.icon} width="50px" height="50px" alt="weather icon" />
                 <p>{this.props.day.applicable_date}</p>
+                <p>Current-Temp: {this.state.farTemps[2]}°F</p>
+                <div style={{float: "left", fontSize: ".7rem", marginRight: "5px", border: "1px solid black", padding: "3px"}}>
+                    <p>Min-Temp: {this.state.farTemps[0]}°F</p>
+                    <p>Max-Temp: {this.state.farTemps[1]}°F</p>
+                </div>
+                <div style={{float: "right", fontSize: ".7rem", border: "1px solid black", padding: "3px"}}>
+                    <p>Humidity: {this.props.day.humidity}%</p>
+                    <p>Wind Speed: {Math.round(this.props.day.wind_speed)}</p>
+                </div>
             </div>
         )
     }
