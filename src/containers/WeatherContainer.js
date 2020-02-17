3bf66eef8a9df9e6fbe3ba9500ab5decb8d8daf9
Nick Boober <nboober@terpmail.umd.edu>
2020-02-17 13:51:09 -0500

    added key values to the map function in the weather container

--- src/containers/WeatherContainer.js
+++ src/containers/WeatherContainer.js
@@ -9,8 +9,9 @@ class WeatherContainer extends React.Component{
                 <h2>{this.props.weather.title}</h2>
                 <p>Sunrise: {this.props.weather ? this.props.weather.sun_rise.substring(11,19) : null} am</p>
                 <p>Sunset: {this.props.weather ? this.props.weather.sun_set.substring(11,19) : null} pm</p>
+
                 {this.props.weather ? this.props.weather.consolidated_weather.map((day, index) => {
-                    return <Day day={day} icon={this.props.icons[index]}/>
+                    return <Day key={index} day={day} icon={this.props.icons[index]}/>
                 }) : null}
 
             </div>
